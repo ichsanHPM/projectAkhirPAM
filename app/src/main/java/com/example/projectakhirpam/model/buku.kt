d3db89e7fa8caa6429eb2c16c0bae3e4d9d5089c
ichsanHPM <ichsanhpm@gmail.com>
2025-01-23 00:21:21 +0700

    membuat data class buku

---
+++ app/src/main/java/com/example/projectakhirpam/model/buku.kt
@@ -0,0 +1,18 @@
+package com.example.projectakhirpam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class buku(
+    @SerialName("id_buku")
+    val id_buku: String,
+    @SerialName("nama_buku")
+    val nama_buku: String,
+    @SerialName("deskripsi_buku")
+    val deskripsi_buku: String,
+    @SerialName("tanggal_terbit")
+    val tanggal_terbit: String,
+    @SerialName("status_buku")
+    val status_buku: String
+)
