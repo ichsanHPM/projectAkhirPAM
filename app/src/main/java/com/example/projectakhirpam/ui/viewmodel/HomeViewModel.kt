81df4d2769750e3d5bfb15ee953678f7c6cc72a4
ichsanHPM <ichsanhpm@gmail.com>
2025-01-23 15:17:38 +0700

    membuat class HomeViewModel dan fungsi getBk, deleteBk di dalamnya

--- app/src/main/java/com/example/projectakhirpam/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/projectakhirpam/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.projectakhirpam.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.projectakhirpam.model.Buku
+import com.example.projectakhirpam.repository.BukuRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState{
     data class Success(val buku: List<Buku>): HomeUiState()
@@ -8,3 +17,36 @@ sealed class HomeUiState{
     object Loading: HomeUiState()
 }
 
+class HomeViewModel (private val bk: BukuRepository): ViewModel(){
+    var bukuUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getBk()
+    }
+
+    fun getBk(){
+        viewModelScope.launch{
+            bukuUiState = HomeUiState.Loading
+            bukuUiState = try {
+                HomeUiState.Success(bk.getBuku())
+            }catch (e: IOException){
+                HomeUiState.Error
+            }catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteBk(id_buku: String){
+        viewModelScope.launch{
+            try {
+                bk.deleteBuku(id_buku)
+            }catch (e: IOException){
+                HomeUiState.Error
+            }catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+}
