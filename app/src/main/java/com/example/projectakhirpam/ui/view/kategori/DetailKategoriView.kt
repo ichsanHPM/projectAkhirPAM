b392e29bc061de328562e91b48a7d29e465b0e57
ichsanHPM <ichsanhpm@gmail.com>
2025-01-28 04:51:22 +0700

    membuat insert kategori view

---
+++ app/src/main/java/com/example/projectakhirpam/ui/view/kategori/DetailKategoriView.kt
@@ -0,0 +1,163 @@
+package com.example.projectakhirpam.ui.view.kategori
+
+import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.projectakhirpam.ui.customwidget.CustomeTopAppBarr
+import com.example.projectakhirpam.ui.navigasi.DestinasiNavigasi
+import com.example.projectakhirpam.ui.viewmodel.DetailUiState
+import com.example.projectakhirpam.ui.viewmodel.PenyediaViewModel
+import com.example.projectakhirpam.ui.viewmodel.kategori.DetailKategoriUiState
+import com.example.projectakhirpam.ui.viewmodel.kategori.DetailKategoriViewModel
+import kotlinx.coroutines.launch
+
+
+object KategoriDetail : DestinasiNavigasi {
+    override val route = "detail_kategori"
+    override val titleRes = "Detail Kategori"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailKategoriScreen(
+    navigateBack: () -> Unit,
+    id_kategori: String,
+    modifier: Modifier = Modifier,
+    viewModel: DetailKategoriViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    navController: NavHostController
+) {
+    val uiState by viewModel.detailUiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBarr(
+                title = KategoriDetail.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("update_kategori/$id_kategori")
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Update Kategori")
+            }
+        }
+    ) { innerPadding ->
+        DetailKategoriBody(
+            detailKategoriUiState = uiState,
+            onDeleteClick = {
+                coroutineScope.launch {
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun DetailKategoriBody(
+    detailKategoriUiState: DetailKategoriUiState,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when (detailKategoriUiState) {
+        is DetailKategoriUiState.Loading -> {
+            CircularProgressIndicator(modifier = modifier.fillMaxSize())
+        }
+        is DetailKategoriUiState.Error -> {
+            Text(
+                text = detailKategoriUiState.message,
+                color = Color.Red,
+                modifier = modifier.fillMaxSize().wrapContentSize(Alignment.Center)
+            )
+        }
+        is DetailKategoriUiState.Success -> {
+            val kategori = detailKategoriUiState.kategori
+            Column(
+                verticalArrangement = Arrangement.spacedBy(18.dp),
+                modifier = Modifier.padding(12.dp)
+            ) {
+                ComponentDetailKategori(judul = "Nama Kategori", isinya = kategori.nama_kategori)
+                ComponentDetailKategori(judul = "Id Kategori", isinya = kategori.id_kategori)
+                ComponentDetailKategori(judul = "Deskripsi Kategori", isinya = kategori.deskripsi_kategori)
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = onDeleteClick,
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Hapus")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailKategori(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 19.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 19.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
---
+++ app/src/main/java/com/example/projectakhirpam/ui/view/kategori/InsertKategoriView.kt
@@ -0,0 +1,140 @@
+package com.example.projectakhirpam.ui.view.kategori
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhirpam.ui.customwidget.CustomeTopAppBarr
+import com.example.projectakhirpam.ui.navigasi.DestinasiNavigasi
+import com.example.projectakhirpam.ui.viewmodel.InsertUiEvent
+import com.example.projectakhirpam.ui.viewmodel.InsertUiState
+import com.example.projectakhirpam.ui.viewmodel.PenyediaViewModel
+import com.example.projectakhirpam.ui.viewmodel.kategori.InsertKategoriUiEvent
+import com.example.projectakhirpam.ui.viewmodel.kategori.InsertKategoriUiState
+import com.example.projectakhirpam.ui.viewmodel.kategori.InsertKategoriViewModel
+import kotlinx.coroutines.launch
+
+object KategoriEntry : DestinasiNavigasi {
+    override val route = "item_kategori"
+    override val titleRes = "Entry Kategori"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKategoriScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertKategoriViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBarr(
+                title = KategoriEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryKategoriBody(
+            insertKategoriUiState = viewModel.uiState,
+            onKategoriValueChange = viewModel::updateInsertKategoriState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKategori()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryKategoriBody(
+    insertKategoriUiState: InsertKategoriUiState,
+    onKategoriValueChange: (InsertKategoriUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputKategori(
+            insertKategoriUiEvent = insertKategoriUiState.insertUiEvent,
+            onValueChange = onKategoriValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputKategori(
+    insertKategoriUiEvent: InsertKategoriUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertKategoriUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertKategoriUiEvent.nama_kategori,
+            onValueChange = { onValueChange(insertKategoriUiEvent.copy(nama_kategori = it)) },
+            label = { Text("Nama Kategori") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertKategoriUiEvent.deskripsi_kategori,
+            onValueChange = { onValueChange(insertKategoriUiEvent.copy(deskripsi_kategori = it)) },
+            label = { Text("Deskripsi Kategori") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
