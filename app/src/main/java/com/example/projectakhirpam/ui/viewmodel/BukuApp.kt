8841b4761e473a281dd6e2c8c5a6b12f26d381c4
ichsanHPM <ichsanhpm@gmail.com>
2025-01-28 04:21:10 +0700

    pemanggilan ic_connection_error

---
+++ app/src/main/java/com/example/projectakhirpam/ui/viewmodel/BukuApp.kt
@@ -0,0 +1,30 @@
+package com.example.projectakhirpam.ui.viewmodel
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.projectakhirpam.ui.navigasi.PengelolaHalaman
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BukuApp(){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}
